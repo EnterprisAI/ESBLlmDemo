2784a6972aea216db6bc1cb7bbf8c5482bbe262b
Vivid-Vortex <ctntc9.dpk@gmail.com>
2025-06-15 14:32:13 +0530

    init

---
+++ src/main/java/com/esb/llm/ESBLlmDemo/EsbLlmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.esb.llm.ESBLlmDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsbLlmDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsbLlmDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esb/llm/ESBLlmDemo/EsbLlmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esb.llm.ESBLlmDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsbLlmDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
